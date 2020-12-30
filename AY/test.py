1706168cd1862416a6b66bc37dd14f59c5a318ad
Yuan Liu <ee07b238@gmail.com>
2020-12-31 01:25:10 +1300

    A test commit

--- AY/test.py
+++ AY/test.py
@@ -8,7 +8,8 @@ st.QA_SU_save_trade_date_all()
 
 st.QA_SU_save_finacial_inicator_data()
 '''
-# st.QA_SU_save_trade_date_all()
+st.QA_SU_save_trade_date_all()
 # st.QA_SU_save_daily_basic()
 # ROA_PE_filter.send_Message()
 st.QA_SU_save_balance_sheet()
+
